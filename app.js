51bd2d7f10357f3ba183f62d2ff582d3c2e0a2c5
Amar Singh <amar071710@gmail.com>
2021-08-06 00:30:30 +0530

    connect mongo

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
-
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
-
-mongoose.connect('mongodb://localhost:27017/art-gallery', {
+const MongoDBStore = require('connect-mongo');
+//const dburl = process.env.DB_URL;
+const dburl = 'mongodb://localhost:27017/art-gallery';
+mongoose.connect(dburl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
@@ -43,6 +44,7 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 
 const sessionConfig = {
+    store: MongoDBStore.create({ mongoUrl: dburl, touchAfter: 24 * 60 * 60 }),        
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
